828bb9b770af0a168eb6b15e18e81727514592e9
diouc@amazon.com <cheikhdiouf@Cheikhs-MacBook-Pro.local>
2022-05-28 03:28:32 -0400

    added facebook login

--- src/App.js
+++ src/App.js
@@ -5,36 +5,21 @@ import { Footer } from "./logincomponents/Footer";
 import { SignInHeader } from "./logincomponents/SignInHeader";
 import { SignInFooter } from "./logincomponents/SignInFooter";
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export function App({ signOut, user }) {
+  return (
+    <main>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </main>
+  );
 }
 
 export default withAuthenticator(App, {
