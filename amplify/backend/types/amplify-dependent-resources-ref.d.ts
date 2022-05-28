c6cfe170aa5095c478758ac5a3e04cd7ca805d4a
diouc@amazon.com <cheikhdiouf@Cheikhs-MacBook-Pro.local>
2022-05-28 13:42:48 -0400

    removed facebook login to allow phone number use

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,44 +3,22 @@ export type AmplifyDependentResourcesAttributes = {
         "mwicnyapp": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string",
-            "CreatedSNSRole": "string",
-            "FacebookWebClient": "string"
-        },
-        "userPoolGroups": {
-            "MembersGroupRole": "string",
-            "TeachersGroupRole": "string",
-            "AdminsGroupRole": "string",
-            "SuperAdminsGroupRole": "string"
-        }
-    },
-    "api": {
-        "AdminQueries": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
+            "CreatedSNSRole": "string"
         }
     },
     "function": {
-        "AdminQueries6bdccc42": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "mwicnyappCustomMessage": {
+        "mwicnyappPostConfirmation": {
             "Name": "string",
             "Arn": "string",
             "LambdaExecutionRole": "string",
             "Region": "string"
         },
-        "mwicnyappPostConfirmation": {
+        "mwicnyappPostAuthentication": {
             "Name": "string",
             "Arn": "string",
             "LambdaExecutionRole": "string",
