9af5a51d60478345cb2675f8e8efe09e30622552
diouc@amazon.com <cheikhdiouf@Cheikhs-MacBook-Pro.local>
2022-05-28 02:33:08 -0400

    added facebook login

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,7 +10,41 @@ export type AmplifyDependentResourcesAttributes = {
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string",
-            "CreatedSNSRole": "string"
+            "CreatedSNSRole": "string",
+            "FacebookWebClient": "string"
+        },
+        "userPoolGroups": {
+            "MembersGroupRole": "string",
+            "TeachersGroupRole": "string",
+            "AdminsGroupRole": "string",
+            "SuperAdminsGroupRole": "string"
+        }
+    },
+    "api": {
+        "AdminQueries": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "function": {
+        "AdminQueries6bdccc42": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "mwicnyappCustomMessage": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "mwicnyappPostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
         }
     }
 }
